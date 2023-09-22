241b215fa7a39556e9b3598d5a3662b3b60d56dd
Muhammad Yusuf <97136620+Yusufparker@users.noreply.github.com>
2023-09-22 09:00:18 +0700

    Update openai.js

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -12,27 +12,28 @@ try {
 try {
    // Buat objek OpenAI dengan menggunakan variabel lokal
    const openai = new OpenAI({ apiKey });
-  
-} catch (error) {
-     console.log('error2',error)
-}
-
-
 // Fungsi untuk mengirim pesan ke OpenAI
-export async function sendMsgToOpenAi(message) {
-   try {
-      const res = await openai.completions.create({
-         model: 'text-davinci-003',
-         prompt: message,
-         temperature: 0.7,
-         max_tokens: 100,
-         top_p: 1,
-         frequency_penalty: 0,
-         presence_penalty: 0,
-      });
-
-      return res.choices[0].text;
+   export async function sendMsgToOpenAi(message) {
+      try {
+         const res = await openai.completions.create({
+            model: 'text-davinci-003',
+            prompt: message,
+            temperature: 0.7,
+            max_tokens: 100,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+         });
+   
+         return res.choices[0].text;
    } catch {
       return 'Ada masalah di API, mungkin kuota gratisannya habis.';
    }
 }
+  
+} catch (error) {
+     console.log('error2',error)
+   return(error)
+}
+
+
