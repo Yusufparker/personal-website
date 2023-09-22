3ed9ae37154a26e320c718fc9b01cde7eb0f7120
Muhammad Yusuf <97136620+Yusufparker@users.noreply.github.com>
2023-09-22 09:01:45 +0700

    Update openai.js

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -3,37 +3,29 @@ import { OpenAI } from "openai";
 
 // Buat variabel lokal untuk menyimpan nilai variabel lingkungan `OPENAI_API_KEY`
 
-try {
-   const apiKey = process.env.OPENAI_API_KEY;
-} catch (error) {
-  console.log(error
-}
 
-try {
-   // Buat objek OpenAI dengan menggunakan variabel lokal
-   const openai = new OpenAI({ apiKey });
-// Fungsi untuk mengirim pesan ke OpenAI
-   export async function sendMsgToOpenAi(message) {
-      try {
-         const res = await openai.completions.create({
-            model: 'text-davinci-003',
-            prompt: message,
-            temperature: 0.7,
-            max_tokens: 100,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-         });
-   
-         return res.choices[0].text;
-   } catch {
-      return 'Ada masalah di API, mungkin kuota gratisannya habis.';
-   }
-}
-  
-} catch (error) {
-     console.log('error2',error)
-   return(error)
-}
+const apiKey = process.env.OPENAI_API_KEY;
 
+
+
+const openai = new OpenAI({ apiKey });
+
+export async function sendMsgToOpenAi(message){
+    try {
+    const res = await openai.completions.create({
+        model: 'text-davinci-003',
+        prompt: message,
+        temperature: 0.7,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    return res.choices[0].text;
+  } catch {
+    return 'Ada masalah di api hehe, mungkin keabisan kuota gratisannya..'
+
+  }
+}
 
